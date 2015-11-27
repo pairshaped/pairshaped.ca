89d657859f0b494c1f9064fa9f5b180d93ad5c87
Dave Rapin <dave@rapin.com>
2015-11-27 14:50:31 -0500

    Added mina for deployment

--- Gemfile
+++ Gemfile
@@ -3,13 +3,10 @@
 source 'https://rubygems.org'
 
 gem 'middleman', '~> 3.3.12'
-gem 'middleman-gh-pages', '~> 0.0.3'
 gem 'middleman-slim', '~> 0.2.2'
 
 gem 'middleman-syntax', '~> 2.0.0' #, '~> 0.0.1'
 gem 'redcarpet', '~> 3.3.2'
-# gem 'kramdown', '~> 1.8'
-# gem 'middleman-syntax', '~> 2.0'
 
 # Live-reloading plugin
 gem 'middleman-livereload', '~> 3.1.0'
---
+++ config/deploy.rb
@@ -0,0 +1,71 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :domain, 'pairshaped.com'
+set :deploy_to, '/var/www/pairshaped'
+set :repository, 'git@github.com:pairshaped/pairshaped.ca.git'
+set :branch, 'master'
+
+# For system-wide RVM install.
+#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, []
+
+# Optional settings:
+set :user, 'deploy'    # Username in the server to SSH to.
+set :forward_agent, true     # SSH forward_agent.
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+end
+
+task :build do
+  queue 'bundle exec middleman build'
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  to :before_hook do
+    # Put things to run locally before ssh
+  end
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'build'
+    invoke :'deploy:cleanup'
+  end
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
